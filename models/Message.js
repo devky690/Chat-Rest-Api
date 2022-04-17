45a24cfb485c5601c2dd89457e5a2a2ade8c841f
Kyle Abrahams <kyleabrahams@Kyles-MacBook-Air.local>
2022-04-16 22:58:10 -0400

    models finished

---
+++ models/Message.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+//ref wont populate immediately
+
+const messageSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  //no array here since not referring to category name but just
+  //id itself
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  //dont need this we can just inside recipe collection with model....query for a recipe id and query against the category id in the parameter seen in the url
+  //to display all the recipes
+  //recipes_id: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 8080;
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI);
+
+const db = mongoose.connection;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("hello");
