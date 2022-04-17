7c1748e6b284efb694aefbbf3b66a8e2e8491b41
Kyle Abrahams <kyleabrahams@Kyles-MacBook-Air.local>
2022-04-17 15:17:16 -0400

    modified model

--- models/Message.js
+++ models/Message.js
@@ -6,7 +6,7 @@ const messageSchema = new mongoose.Schema({
   content: { type: String, required: true },
   //no array here since not referring to category name but just
   //id itself
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  username: { type: String, required: true },
   //dont need this we can just inside recipe collection with model....query for a recipe id and query against the category id in the parameter seen in the url
   //to display all the recipes
   //recipes_id: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
